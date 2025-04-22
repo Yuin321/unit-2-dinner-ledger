8a550b929d23c998d8496ec8a8bb227389835ee3
Yuin <cxy856@uowmail.edu.au>
2025-04-22 12:13:00 +1000

    Updated anonymous signin

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
+import { getAuth, signInAnonymously } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -21,5 +19,13 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+signInAnonymously(auth)
+  .then(() => {
+    console.log("✅ Signed in anonymously");
+  })
+  .catch((error) => {
+    console.error("❌ Anonymous sign-in error:", error);
+  });
+export { auth, db };
